b03705c9b6cdff28658a0832f7472cf7133313eb
jungbumwoo <jungbumwoo@naver.com>
2021-06-22 20:39:13 +0900

    delete backend

--- backend/src/routers/notInterestRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { postDeleteNotInt } from "../controllers/notInterestController";
-import { requireSignin } from "../middlewares";
-
-const router = express.Router();
-
-router.post("/delete/notinterest", requireSignin, postDeleteNotInt);
-
-export default router;
